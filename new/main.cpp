fd6c07384c69d4d903b108a8801c1ef8641a0963
zrlyx <20141105042hyb@admindeiMac-007.local>
2015-11-25 18:03:51 +0800

    Initial Commit

---
+++ new/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  new
+//
+//  Created by  20141105042hyb on 15/11/25.
+//  Copyright (c) 2015年 20141105042HYB. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
